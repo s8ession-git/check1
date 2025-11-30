22fe4ebee0fc5dff4b55530d7dabd57064eaf549
s8ession <s8ession@gmail.com>
2025-11-30 16:16:35 +0300

    Update my_module.py
    -- Нужно с кодировкой что-то сделать. Windows-1251 пишет. Русские буквы UTF-8 не понимает...
    Либо писать в другом формате текст изначально, либо кодировку для текста при записи менять.
    
    На подумать.

--- my_module.py
+++ my_module.py
@@ -6,7 +6,7 @@ class FileLogger:
         self.filename = filename
         self.logger = logging.getLogger(filename)
         self.logger.setLevel(logging.INFO)
-        self.handler = logging.FileHandler(filename, encoding='utf-8')
+        self.handler = logging.FileHandler(filename)
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt= '%d-%m-%Y %H:%M:%S')
         self.handler.setFormatter(formatter)
         self.logger.addHandler(self.handler)
